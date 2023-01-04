5e7d6e49b74ff9d4a5b9bbaace23b200367e11c8
Akito Iguchi <guchii0307@gmail.com>
2023-01-04 09:24:25 +0900

    add sns link

--- blog-front/src/components/common/header.tsx
+++ blog-front/src/components/common/header.tsx
@@ -5,6 +5,9 @@ import { Box } from "@mui/system";
 
 import { Avatar, useTheme, useMediaQuery } from "@mui/material";
 import { Typography } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { IconButton } from "@mui/material";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 export const Header: React.FC = () => {
   const data = useStaticQuery<Queries.HeaderQuery>(graphql`
@@ -22,13 +25,7 @@ export const Header: React.FC = () => {
   const matches = useMediaQuery(theme.breakpoints.down("md"));
 
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      justifyContent="space-around"
-      my={2}
-      mx="auto"
-    >
+    <Box display="flex" justifyContent="space-around" my={2} mx="auto">
       {data.file?.childImageSharp && (
         <Avatar
           src={getSrc(data.file.childImageSharp)}
@@ -39,15 +36,24 @@ export const Header: React.FC = () => {
         />
       )}
       <Box mx={2}>
-        <Typography
-          component="h1"
-          fontSize={theme.breakpoints.up("md") ? "2rem" : "1.75rem"}
-          my={1}
-        >
-          ぐっちー
-        </Typography>
+        <Box display="flex" justifyContent="space-between">
+          <Typography
+            component="h1"
+            fontSize={theme.breakpoints.up("md") ? "2rem" : "1.75rem"}
+          >
+            ぐっちー
+          </Typography>
+          <Box>
+            <IconButton href="https://github.com/Furafrafrfr" target="_blank">
+              <GitHubIcon sx={{ color: "#24292f" }} />
+            </IconButton>
+            <IconButton href="https://twitter.com/Furafrafrfr" target="_blank">
+              <TwitterIcon sx={{ color: "#1D9BF0" }} />
+            </IconButton>
+          </Box>
+        </Box>
         <Typography component="p" variant="body2">
-          経済学部の大学生をやっています。ゲームをしたり本を読んだりプログラミングをしたりしています。C#とかReactとかGatsby.jsが好きです。
+          経済学部の大学生です。ゲームをしたり本を読んだりプログラミングをしたりしています。C#とかReactとかGatsby.jsが好きです。
         </Typography>
       </Box>
     </Box>
